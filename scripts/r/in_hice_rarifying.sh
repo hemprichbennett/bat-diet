5abe91951ed77e6a4b2e244087e57f3f999c3c41
hemprichbennett <hemprich.bennett@gmail.com>
2018-05-21 17:36:19 +0100

    Ramped up the length of time that the rarifying requests

--- scripts/r/in_hice_rarifying.sh
+++ scripts/r/in_hice_rarifying.sh
@@ -4,7 +4,7 @@
 #$ -j y     # Maximum output, inc errors
 #$ -r y     # Condense error files
 #$ -pe smp 1     # Request CPU cores
-#$ -l h_rt=100:0:0     # Request runtime (up to 240 hours)
+#$ -l h_rt=200:0:0     # Request runtime (up to 240 hours)
 #$ -l h_vmem=20G     # Request RAM per core
 #$ -m bea     # Status emails
 
