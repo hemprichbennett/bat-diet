7477011f313846df6352a64cab6e0a24e509c9d7
hemprichbennett <hemprich.bennett@gmail.com>
2018-05-24 10:42:38 +0100

    Set it up for knitr a bit more

--- scripts/r/otu_conclusion_check_output_use.R
+++ scripts/r/otu_conclusion_check_output_use.R
@@ -1,8 +1,10 @@
+#This script uses the output from the script otu_conclusion_check_generation.R
 ## @knitr setup
 library(here())
 library(LOTUS)
 library(ggplot2)
-setwd(here())
+library(gridExtra)
+#setwd(here())
 
 firstup <- function(x) {
   substr(x, 1, 1) <- toupper(substr(x, 1, 1))
@@ -28,11 +30,11 @@ sitescatter <- ggplot(m , aes(x = clustering, y = value, color = site)) +
   facet_wrap(~ firstup(gsub('\\.', ' ', metric)), scales = 'free_y')+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_blank(), axis.line = element_line(colour = "black"))
-print(sitescatter)
-pdf('plots/Site comparisons/MOTU_sitescatter.pdf')
-sitescatter
-dev.off()
-##
+#print(sitescatter)
+#pdf('plots/Site comparisons/MOTU_sitescatter.pdf')
+#sitescatter
+#dev.off()
+
 netscatter <- ggplot(m , aes(x = clustering, y = value, color = network)) +
   geom_point()+
   labs(x = 'clustering') +
@@ -42,11 +44,11 @@ netscatter <- ggplot(m , aes(x = clustering, y = value, color = network)) +
   facet_wrap(~ firstup(gsub('\\.', ' ', metric)), scales = 'free_y')+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_blank(), axis.line = element_line(colour = "black"))
-print(netscatter)
-pdf('plots/Site comparisons/MOTU_netscatter.pdf')
-netscatter
-dev.off()
-##
+#print(netscatter)
+#pdf('plots/Site comparisons/MOTU_netscatter.pdf')
+#netscatter
+#dev.off()
+
 
 habscatter <- ggplot(m , aes(x = clustering, y = value, color = habitat_type)) +
   geom_point()+
@@ -57,15 +59,30 @@ habscatter <- ggplot(m , aes(x = clustering, y = value, color = habitat_type)) +
   facet_wrap(~ firstup(gsub('\\.', ' ', metric)), scales = 'free_y')+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_blank(), axis.line = element_line(colour = "black"))
-print(habscatter)
-pdf('plots/Site comparisons/MOTU_habscatter.pdf')
-habscatter
-dev.off()
-
+#print(habscatter)
+#pdf('plots/Site comparisons/MOTU_habscatter.pdf')
+#habscatter
+#dev.off()
+## @knitr all_scatterplots
+grid.arrange(netscatter, sitescatter, habscatter, ncol = 2)
+## @knitr lineplot
 #motuline <- line_plot(input = m, network = 'network', clustering = 'clustering', metric = 'metric', value = 'value', plotname = 'Sabah')
-pdf('plots/Site comparisons/MOTU_line.pdf')
+#pdf('plots/Site comparisons/MOTU_line.pdf')
+#line_plot(input = m, network = 'network', clustering = 'clustering', metric = 'metric', value = 'value', plotname = 'Sabah')
+#dev.off()
 line_plot(input = m, network = 'network', clustering = 'clustering', metric = 'metric', value = 'value', plotname = 'Sabah')
-dev.off()
 #####ANOVA ####
+## @knitr anova
 m$network <- as.factor(m$network)
 aov(aov(value ~ clustering + network + clustering:network, data = m[m$metric=='web asymmetry',]))
+
+
+## @knitr funct_comp_scatter
+ggplot(m[m$metric=='functional.complementarity.HL',] , aes(x = clustering, y = value, color = network)) +
+  geom_point()+
+  labs(x = 'Clustering', y= 'Fucntional complementarity') +
+  geom_smooth(method = lm, se = T)+
+  scale_x_continuous(breaks = seq(91, 98, 1))+
+  #scale_color_manual(values=c('#E69F00', '#56B4E9'))+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"))
