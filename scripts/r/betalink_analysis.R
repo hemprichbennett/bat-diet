16553c6a5b2dbf67d6f234a709238df34775bce2
hemprichbennett <hemprich.bennett@gmail.com>
2018-05-28 18:27:07 +0100

    Plotted the betavalues

--- scripts/r/betalink_analysis.R
+++ scripts/r/betalink_analysis.R
@@ -16,14 +16,9 @@ if(grepl('data', basedir)){
   library('here')
   library(ggplot2)
   library(tidyverse)
-  library(ggridges)
-  library(gridExtra)
-  library(forcats)
   library(reshape2)
-  library(corrplot)
-  library(iNEXT)
-  library(DataExplorer)
   library(betalink)
+  library(forcats)
 }
 
 setwd(here())
@@ -31,10 +26,31 @@ source('scripts/r/r_network_gen.r')
 
 
 nets <- r_network_gen(collapse_species = T, filter_species = T, lulu = T, include_malua = F)
-
+names(nets) <- gsub('.+ ^', '.+^', names(nets))
 graphs <- prepare_networks(nets)
 
 
-network_betadiversity(graphs)
+beta <- network_betadiversity(graphs)
+#Making a new object to allow prettier plotting
+temp <- beta
+colnames(temp)[c(1,2)] <- c('j', 'i')
+for_plot <- rbind(beta, temp)
+#Order the factors so that the plot looks nice
+for_plot$i <- ordered(for_plot$i, levels = c("DANUM, 2016", "DANUM, 2017", "MALIAU, 2016", 'MALIAU, 2017',
+                                             'SAFE, 2015', 'SAFE, 2016', 'SAFE, 2017'))
+for_plot$j <- ordered(for_plot$j, levels = c("DANUM, 2016", "DANUM, 2017", "MALIAU, 2016", 'MALIAU, 2017',
+                                             'SAFE, 2015', 'SAFE, 2016', 'SAFE, 2017'))
+melted_forplot <- melt(for_plot)
 
-#metaweb(graphs)
+#str(beta)
+#for_plot$j <- fct_rev(for_plot$j)
+betaplot <- ggplot(melted_forplot, aes(i, fct_rev(j)))+ geom_tile(aes(fill=value), colour = 'white')+
+  scale_fill_gradient(low = "white",
+                      high = "black", limits = c(0,1))+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+          panel.background = element_blank(), axis.line = element_line(colour = "black"))+
+  facet_wrap(~ variable)
+  
+pdf('plots/betaplot.pdf')
+betaplot
+dev.off()
