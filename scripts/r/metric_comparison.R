d8262fce42bc6b07eadba02759fe4eebddfeaa2b
hemprichbennett <hemprich.bennett@gmail.com>
2018-05-30 13:55:18 +0100

    Makes a nice facetted plot, but its wrong

--- scripts/r/metric_comparison.R
+++ scripts/r/metric_comparison.R
@@ -28,6 +28,17 @@ setwd(here())
 source('scripts/r/r_network_gen.r')
 source('scripts/r/hernani_comparisons.R')
 
+signif_check <- function(networks, mars= 'both',mtype="count", index, level = 'higher'){
+  if(length(index)>1){stop('this function fucks up if you use more than one index at a time. Sorry')}
+  random <- lapply(networks, function(x) replicate(1000, bipartite::networklevel(vegan::permatfull(x, fixedmar= mars, mtype="count",times=1)$perm[[1]],
+                                                                        index = index, level = level)))
+  quantiles <- lapply(random, function(x) quantile(x, probs=c(0.025, 0.975)))
+  real <- lapply(nets, function(x) networklevel(x, index = index, level = level))
+  out <- list(random, quantiles, real)
+  names(out) <- c('random', 'quantiles', 'real')
+  return(out)
+}
+
 nets <- r_network_gen(collapse_species = T, desired_species = NULL, filter_species = T,
                       include_malua = F, lulu= T)
 
@@ -41,5 +52,31 @@ vals <- lapply(nets, function(x) networklevel(x, index = desired_mets, level = '
 df <- data.frame(do.call(rbind, vals))
 
 
-randomized_ranges(networks = nets, input_format = 'clust_only', indices = 'connectance', network_level = 'higher',
-                  sums_to_preserve = 'both')
+random <- lapply(nets, function(x) replicate(1000, bipartite::networklevel(vegan::permatfull(x, fixedmar= 'both',mtype="count",times=1)$perm[[1]],
+                                        index = 'connectance', level = 'higher')))
+
+
+rbind(sapply(b, function(x) quantile(x, probs=c(0.025, 0.975))),
+sapply(nets, function(x) networklevel(x, index = 'connectance')))
+
+
+
+fun_comp <- signif_check(nets, index = c('functional complementarity'), level = 'higher')
+niche <- signif_check(nets, index = c('niche overlap'), level = 'higher')
+
+melted_fun_comp <- melt.list(fun_comp)
+melted_fun_comp$metric <- rep('functional complementarity', nrow(melted_fun_comp))
+melted_niche <- melt.list(niche)
+melted_niche$metric <- rep('niche overlap', nrow(melted_niche))
+sigs <- rbind(melted_fun_comp, melted_niche)
+str(sigs)
+colnames(sigs) <- c('value', 'network', 'list item', 'metric')
+sigs$network <- as.factor(sigs$network)
+sigs$`list item` <- as.factor(sigs$`list item`)
+sigs$metric <- as.factor(sigs$metric)
+
+ggplot(sigs[which(sigs$`list item`=='random'),], aes(value)) + 
+  geom_histogram(binwidth = 0.5) +facet_grid(network ~ metric, scales = 'free')+
+  geom_vline(data=sigs[which(sigs$`list item`=='real'),], aes(xintercept = value), colour="red")+
+  geom_vline(data=sigs[which(sigs$`list item`=='quantiles'),], aes(xintercept = value), colour="blue")
+  
