9a3b02da02415af7aeb6509dbca47324dacde4e5
hemprichbennett <hemprich.bennett@gmail.com>
2018-06-16 20:09:48 +0100

    Increased RAM allocation after job failed

--- scripts/r/in_a_netreducing.sh
+++ scripts/r/in_a_netreducing.sh
@@ -5,7 +5,7 @@
 #$ -r y     # Condense error files
 #$ -pe smp 1     # Request CPU cores
 #$ -l h_rt=240:0:0     # Request runtime (up to 240 hours)
-#$ -l h_vmem=50G     # Request RAM per core
+#$ -l h_vmem=80G     # Request RAM per core
 #$ -m bea     # Status emails
 #$ -t 1-11
 
