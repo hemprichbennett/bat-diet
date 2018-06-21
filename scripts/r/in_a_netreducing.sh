2c45f27e78399aa9cdbb53ce2acb3f2f58590044
hemprichbennett <hemprich.bennett@gmail.com>
2018-06-21 14:35:43 +0100

    Increased time allocation

--- scripts/r/in_a_netreducing.sh
+++ scripts/r/in_a_netreducing.sh
@@ -4,7 +4,7 @@
 #$ -j y     # Maximum output, inc errors
 #$ -r y     # Condense error files
 #$ -pe smp 1     # Request CPU cores
-#$ -l h_rt=10:0:0     # Request runtime (up to 240 hours)
+#$ -l h_rt=40:0:0     # Request runtime (up to 240 hours)
 #$ -l h_vmem=10G     # Request RAM per core
 #$ -m bea     # Status emails
 #$ -t 1-11
