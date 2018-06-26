c5e37c3f9a506531430b2fcbf8034f9e646c6065
hemprichbennett <hemprich.bennett@gmail.com>
2018-06-26 19:32:40 +0100

    Made models for degree ~ site, added some headers

--- scripts/r/specieslevel.R
+++ scripts/r/specieslevel.R
@@ -9,13 +9,18 @@
 library(here)
 library(ggplot2)
 library(magrittr)
+library(reshape2)
 
 source(here('scripts', 'r', 'r_network_gen.r'))
 
 nets <- r_network_gen(lulu = T, filter_species = T)
 
+
+#####Do the actual calculations ####
 sp_mets <- lapply(nets, function(x) specieslevel(x, level = 'higher'))
 
+
+####Format the data ####
 sp_df <- do.call(rbind, sp_mets)
 sp_df$site <- gsub('\\..+', '', rownames(sp_df))
 sp_df$species <- gsub('.+\\.', '', rownames(sp_df))
@@ -43,15 +48,33 @@ ggplot(sp_df, aes(x=site, y = degree))+ geom_point()+ facet_wrap(~ species)+
   theme_bw() + theme(panel.border = element_blank(), panel.grid.major = element_blank(),panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
 
 
-#Now calculate the degree distribution of the networks
-degree_dist <- melt(lapply(nets, function(x) rowSums(x)))
+####Now calculate the degree distribution of the networks####
+
+#Make individual-based networks
+
+ind_nets <- r_network_gen(collapse_species = F, lulu = T, filter_species = T)
+ind_nets <- ind_nets[,-1] #These are the rownames but we dont need them
+
+ind_nets <- ind_nets[,-which(!ind_nets[1,] %in% c('SAFE', 'DANUM', 'MALIAU'))]
+
+ind_netlist <- list()
+
+for(i in 1:length(unique(as.character(ind_nets[1,])))){
+  site <- unique(as.character(ind_nets[1,]))[i]
+  print(site)
+  mat <- ind_nets[3:nrow(ind_nets),which(ind_nets[1,]==site)]
+  ind_netlist[[site]] <- apply(mat, 2, as.numeric)
+}
+
+degree_dist <- melt(lapply(ind_netlist, function(x) colSums(x)))
 colnames(degree_dist) <- c('Degree', 'Site')
 
 ggplot(degree_dist, aes(x=Degree))+ geom_histogram(binwidth = 1)+ facet_wrap(~Site)+
   theme_bw() + theme(panel.border = element_blank(), panel.grid.major = element_blank(),panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
 
+fit <- aov(Degree ~ Site, degree_dist)
 
+plot(fit)
 
-
-
+TukeyHSD(fit)
   
