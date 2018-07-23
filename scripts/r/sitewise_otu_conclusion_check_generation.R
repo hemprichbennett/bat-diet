0f5548c0d1489f4008b7d8fe0628de378565267b
hemprichbennett <hemprich.bennett@gmail.com>
2018-07-23 16:07:24 +0100

    Got rid of lots of unnecessary scatterplots, improved lineplot formatting

--- scripts/r/sitewise_otu_conclusion_check_generation.R
+++ scripts/r/sitewise_otu_conclusion_check_generation.R
@@ -213,37 +213,40 @@
   #m$year <- unlist(lapply(m$network, function(x) strsplit(as.character(x), split = ', ')[[1]][2]))
   m$site <- gsub('DANUM', 'Danum', m$site)
   m$site <- gsub('MALIAU', 'Maliau', m$site)
-  m$habitat_type <- as.factor(ifelse(m$site == 'SAFE', 'Logged', 'Primary'))
-  m$metric <- gsub(' ', '\n', m$metric)
-  m$metric <- gsub('\\.', '\n', m$metric)
-  m$metric <- gsub('\nHL', '', m$metric)
-  
-  
-  sitescatter <- ggplot(m , aes(x = clustering, y = value, color = site)) +
-    geom_point()+
-    labs(x = 'Clustering (%)', y = NULL) +
-    geom_smooth(method = lm, se = T)+
-    scale_x_continuous(breaks = seq(91, 98, 1))+
-    scale_color_brewer(type = 'qual')+
-    facet_wrap(~ firstup(gsub('\\.', ' ', metric)), scales = 'free')+
-    theme(strip.background = element_rect(fill="white"), strip.placement = "outside", panel.spacing = unit(0.8, "lines"))+#strip stuff sorts the facet labels, spacing adjusts the space between facets
-    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-          panel.background = element_blank(), axis.line = element_line(colour = "black"))
-## @knitr otu_scatterplot
-  sitescatter
-  
+  # m$habitat_type <- as.factor(ifelse(m$site == 'SAFE', 'Logged', 'Primary'))
+  # m$metric <- gsub(' ', '\n', m$metric)
+  # m$metric <- gsub('\\.', '\n', m$metric)
+  # m$metric <- gsub('\nHL', '', m$metric)
+  # 
+  # m$metric <- gsub('mean\nnumber\nof\nshared\npartners', 'mean number of\nshared partners', m$metric)
+  m$metric <- gsub('\\.', ' ', m$metric)
+  m$metric <- gsub(' HL', '', m$metric)
+  m$metric <- firstup(m$metric)
+#   sitescatter <- ggplot(m , aes(x = clustering, y = value, color = site)) +
+#     geom_point()+
+#     labs(x = 'Clustering (%)', y = NULL) +
+#     geom_smooth(method = lm, se = T)+
+#     scale_x_continuous(breaks = seq(91, 98, 1))+
+#     scale_color_brewer(type = 'qual')+
+#     facet_wrap(~ firstup(gsub('\\.', ' ', metric)), scales = 'free')+
+#     theme(strip.background = element_rect(fill="white"), strip.placement = "outside", panel.spacing = unit(0.8, "lines"))+#strip stuff sorts the facet labels, spacing adjusts the space between facets
+#     theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+#           panel.background = element_blank(), axis.line = element_line(colour = "black"))
+# ## @knitr otu_scatterplot
+#   sitescatter
+#   
 ## @knitr otu_lineplot
   
   line_plot(input = m, metric = 'metric', network = 'network', clustering = 'clustering', value = 'value')  
   
 ## @knitr plot_saving  
-  sitescatter
-  pdf('plots/MOTU_sites_combined.pdf')
-  sitescatter
-  dev.off()
-  jpeg('plots/MOTU_sites_combined.jpg', width = 7, height = 7, units = 'in', res = 300)
-  sitescatter
-  dev.off()
+  # sitescatter
+  # pdf('plots/MOTU_sites_combined.pdf')
+  # sitescatter
+  # dev.off()
+  # jpeg('plots/MOTU_sites_combined.jpg', width = 7, height = 7, units = 'in', res = 300)
+  # sitescatter
+  # dev.off()
 
   
   jpeg('plots/sitewise_lineplot.jpg', width = 7, height = 7, units = 'in', res = 300)
@@ -253,15 +256,15 @@
   pdf('plots/sitewise_lineplot.pdf')
   line_plot(input = m, metric = 'metric', network = 'network', clustering = 'clustering', value = 'value')  
   dev.off()
-  
-  
-  ggplot(m[m$metric=='NODF',] , aes(x = clustering, y = value, color = network)) +
-    geom_point()+
-    labs(x = 'Clustering (%)', y = NULL) +
-    geom_smooth(method = lm, se = T)+
-    scale_x_continuous(breaks = seq(91, 98, 1))+
-    scale_color_brewer(type = 'qual')+
-    theme(strip.background = element_rect(fill="white"), strip.placement = "outside", panel.spacing = unit(0.8, "lines"))+#strip stuff sorts the facet labels, spacing adjusts the space between facets
-    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-          panel.background = element_blank(), axis.line = element_line(colour = "black"))
-  
+  # 
+  # 
+  # ggplot(m[m$metric=='NODF',] , aes(x = clustering, y = value, color = network)) +
+  #   geom_point()+
+  #   labs(x = 'Clustering (%)', y = NULL) +
+  #   geom_smooth(method = lm, se = T)+
+  #   scale_x_continuous(breaks = seq(91, 98, 1))+
+  #   scale_color_brewer(type = 'qual')+
+  #   theme(strip.background = element_rect(fill="white"), strip.placement = "outside", panel.spacing = unit(0.8, "lines"))+#strip stuff sorts the facet labels, spacing adjusts the space between facets
+  #   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+  #         panel.background = element_blank(), axis.line = element_line(colour = "black"))
+  # 
