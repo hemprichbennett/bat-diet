708c5a1b1dc48fab24b9d562127e38c9b62cadeb
hemprichbennett <hemprich.bennett@gmail.com>
2018-07-24 17:46:51 +0100

    Added code to find which bat species consumed pests

--- scripts/r/bold_output_analysis.R
+++ scripts/r/bold_output_analysis.R
@@ -9,6 +9,7 @@
 library(here)
 library(bold)
 library(ggmap)
+library(magrittr)
 
 
 setwd(here())
@@ -76,12 +77,55 @@ names(nets) <- gsub('DANUM', 'Danum', names(nets))
 names(nets) <- gsub('MALIAU', 'Maliau', names(nets))
 
 
-mat <- matrix(nrow = 0, ncol = length(nets)+2) 
-for(i in 1:length(best)){
-  vec <- c(best[i], names(best)[i],sapply(nets, function(x) best[i] %in% rownames(x)))
-  mat <- rbind(mat, vec)
-  rownames(mat)[i] <- best[i]
+
+for(i in 1:length(nets)){
+  rows <- which(rownames(nets[[i]]) %in% best)
+  otus <- rownames(nets[[i]][which(rownames(nets[[i]]) %in% best),])
+  subnet <- nets[[i]][rows,]
+  #consumption <- 
 }
 
-df <- as.data.frame(mat)
+for(i in 2:2){
+#for(i in 1:lenght(nets)){
+  otus <- best[best %in% rownames(nets[[i]])] # Find the otu matches for this network
+  preysp <- names(best[best %in% rownames(nets[[i]])]) #get their names
+  #make a subnetwork containing only the relevant otus and bats
+  subnet_1 <- nets[[i]][which(rownames(nets[[i]]) %in% otus),]
+  subnet_2 <- subnet_1[,which(colSums(subnet_1)>0)]
+  melt(subnet_2)
+}
+
+
+best_df <- data.frame(best, names(best))
+colnames(best_df)[2] <- 'Pest species'
+
+
+#Format the nets to merge them with the pest data
+edgelist <- melt(nets)
+colnames(edgelist) <- c('otu', 'Bat species', 'Number of bats consuming', 'Site')
+edgelist <- edgelist[edgelist$`Number of bats consuming`>0,]
+edgelist <- edgelist[edgelist$otu %in% best_df$best,]
+
+
+
+consumptiondf <- merge(y = best_df, x = edgelist, by.y = 'best', by.x= 'otu', all.x = T, all.y = F)
+
+consumptiondf$`Bat species` %<>% 
+  gsub('Hice', 'Hipposideros cervinus', .)%<>%
+  gsub('Kein', 'Kerivoula intermedia', .)%<>%
+  gsub('Rhse', 'Rhinolophus sedulus', .)%<>%
+  gsub('Rhtr', 'Rhinolophus trifoliatus', .)%<>%
+  gsub('Hidi', 'Hipposideros diadema', .)#%<>%
+
+consumptiondf$otu <- NULL
+
+consumptiondf <- consumptiondf[,c(4,1,3,2)]
+consumptiondf <- consumptiondf[
+  with(consumptiondf, order(Site, `Bat species`)),
+  ]
+write.csv(consumptiondf, 'results/pest_consumption.csv', row.names = F)
+
+
+#consumptiondf[ order(-consumptiondf[,3], consumptiondf[,2]), ]
+
 
