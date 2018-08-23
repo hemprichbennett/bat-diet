587a52ea1ccadbbe7577744b2c54ad19a962d6b7
hemprichbennett <hemprich.bennett@gmail.com>
2018-08-23 15:18:39 +0100

    Removed superfluous code

--- scripts/r/ranges_analysis.R
+++ scripts/r/ranges_analysis.R
@@ -30,6 +30,8 @@ cbPalette <- c("#5a3fa8",
 
 files <- list.files(path = 'data/output_data/randomized_ranges/', pattern ='.csv')
 files <- paste('data/output_data/randomized_ranges/', files, sep ='')
+
+files <- files[-grep('smaller', files)]
 file_list <- lapply(files, read.csv, row.names= 1)
 
 ranges_df <- do.call(rbind, file_list)
@@ -37,141 +39,45 @@ colnames(ranges_df)[c(4,5)] <- c('lower', 'upper')
 ranges_df$network <- gsub('DANUM', 'Danum', ranges_df$network)
 ranges_df$network <- gsub('MALIAU', 'Maliau', ranges_df$network)
 
+for_facets <- ranges_df
 #Calculate if a value is significant or not
-ranges_df$signif <- rep('significant', nrow(ranges_df))
-for(i in 1:nrow(ranges_df)){
-  if(ranges_df$actual[i] > ranges_df$lower[i] & ranges_df$actual[i] < ranges_df$upper[i]){
-    ranges_df$signif[i] <- 'non significant'
+for_facets$dummy_for_plotting <- for_facets$actual
+for_facets$signif <- 'significant'
+for(i in 1:nrow(for_facets)){
+  if(for_facets$actual[i] > for_facets$lower[i] & for_facets$actual[i] < for_facets$upper[i]){
+    for_facets$signif[i] <- 'non significant'
+    for_facets$actual[i] <- NA
   }
 }
 
-##First plot and output all metrics with all values
-p_list <- list()
-for(i in 1: length(unique(ranges_df$metric))){
-  met <- unique(ranges_df$metric)[i]
-  pdf(paste('plots/randomized_ranges/', met, '.pdf', sep =''))
-  p_list[[met]] <- ggplot(ranges_df[which(ranges_df$metric==met),], aes(clustering, actual, colour = network))+
-    geom_point()+
-    geom_errorbar(aes(ymin=lower, ymax=upper, colour = network), width=.1, alpha = 0.5)+
-    scale_fill_manual(values=cbPalette)+
-    scale_colour_manual(values=cbPalette)+
-    theme_bw()+
-    theme(panel.border = element_blank(), panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
-  print(p_list[[met]])
-  dev.off()
-}
 
-
-#Now make a set of paired plots, showing significance or lack of it
-
-ranges_df$metric <- gsub(' ', '\n', ranges_df$metric)
-# 
-# for(i in 1: length(unique(ranges_df$metric))){
-# 
-#   met <- unique(ranges_df$metric)[i]
-#   
-#  nolegend <- ggplot(ranges_df[which(ranges_df$metric==met),], aes(clustering, actual, colour = network))+
-#     geom_point()+
-#     geom_errorbar(aes(ymin=lower, ymax=upper, colour = network), width=.1, alpha = 0.5)+
-#     scale_fill_manual(values=cbPalette)+
-#     scale_colour_manual(values=cbPalette)+
-#     theme_bw()+
-#     theme(legend.position="none")+
-#     theme(panel.border = element_blank(), panel.grid.major = element_blank(),
-#           panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))+
-#    labs(x = 'Clustering', y = firstup(as.character(met)))
-#   
-# 
-#    only_sig <- ggplot(ranges_df[which(ranges_df$metric==met & ranges_df$signif=='significant'),], aes(clustering, actual, colour = network))+
-#        geom_point()+
-#        scale_fill_manual(values=cbPalette)+
-#        scale_colour_manual(values=cbPalette)+
-#        theme_bw()+
-#        theme(legend.position="none")+
-#        theme(panel.border = element_blank(), panel.grid.major = element_blank(),
-#              panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))+
-#    labs(x = 'Clustering', y = firstup(as.character(met)))
-#  
-#   leg <- g_legend(ggplot(ranges_df[which(ranges_df$metric==met & ranges_df$signif=='significant'),], aes(clustering, actual, colour = network))+
-#                     geom_point()+
-#                     geom_errorbar(aes(ymin=lower, ymax=upper, colour = network), width=.1, alpha = 0.5)+
-#                     scale_fill_manual(values=cbPalette)+
-#                     scale_colour_manual(values=cbPalette)+
-#                     theme_bw()+
-#                     theme(panel.border = element_blank(), panel.grid.major = element_blank(),
-#                           panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")))
-#                   
-#   pdf(paste('plots/randomized_ranges/sig_and_non_', met, '.pdf', sep =''))
-#   grid.arrange(nolegend, only_sig, leg, ncol = 3)
-#     dev.off()
-# }
-# 
-# dev.off()
+#for_facets <- rbind(for_facets, for_sig_only_vals)
+for_facets$metric <- as.character(for_facets$metric)
+for_facets$metric <- sapply(for_facets$metric, firstup)
+for_facets$metric <- gsub(' ', '\n', for_facets$metric)
 
 
 
-
-all <- ggplot(ranges_df, aes(clustering, actual, colour = network))+
-  geom_point()+
-  geom_errorbar(aes(ymin=lower, ymax=upper, colour = network), width=.1, alpha = 0.5)+
-  scale_fill_manual(values=cbPalette)+
-  scale_colour_manual(values=cbPalette)+
-  theme_bw()+
-  theme(panel.border = element_blank(), panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))+
-  labs(x = 'Clustering', y = 'Measured value, random values')+
-  facet_wrap(~ metric, nrow = 1, scales = 'free_y')+
-  theme(legend.position="none")
-  
-
-all_only_sig <- ggplot(ranges_df[which(ranges_df$signif=='significant'),], aes(clustering, actual, colour = network))+
-  geom_point()+
-  scale_fill_manual(values=cbPalette)+
-  scale_colour_manual(values=cbPalette)+
-  theme_bw()+
-  theme(panel.border = element_blank(), panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))+
-  labs(x = 'Clustering threshold', y = 'Measured value')+
-  facet_wrap(~ metric, nrow = 1, scales = 'free_y')+
-  theme(legend.position="none")
-
-grid.arrange(all, all_only_sig, leg, nrow = 3)
-
-
-#Make a df for a facetted version, as thats clearly a better way of doing this
-
-for_sig_only_vals <- ranges_df[which(ranges_df$signif=='significant'),]
-for_sig_only_vals$lower <- NA
-for_sig_only_vals$upper <- NA
-for_sig_only_vals$signif <- 'Only significant values'
-
-for_facets <- ranges_df
-for_facets$signif <- 'Observed and expected values'
-
-
-for_facets <- rbind(for_facets, for_sig_only_vals)
-for_facets$metric <- firstup(for_facets$metric)
 
 #Remove metrics which are uninteresting #
 unique(for_facets$metric)
-for_facets <- for_facets[-which(for_facets$metric %in% c('Alatalo\ninteraction\nevenness', 'Web\nasymmetry', "Fisher\nalpha", "Interaction\nstrength\nasymmetry")),]
+for_facets <- for_facets[-which(for_facets$metric %in% c('Alatalo\ninteraction\nevenness', 'Web\nasymmetry', "Fisher\nalpha", "Interaction\nstrength\nasymmetry", "Togetherness", 'Nestedness', 'Weighted\nnestedness', 'Weighted\nNODF')),]
 
 
 #plot
-metrics_facet <- ggplot(for_facets, aes(y =clustering, x=actual, colour = network))+
-  geom_point()+
-  facet_grid(signif ~ metric, scales = 'free_x')+
-  geom_errorbarh(aes(xmin=lower, xmax=upper, colour = network), height = 0.4, alpha = 0.5)+
-  scale_colour_manual(values=cbPalette, name = 'Network')+
-  scale_linetype_discrete(name = 'Error')+
+metrics_facet <- ggplot(for_facets, aes(y =clustering, x= dummy_for_plotting, colour = network))+
+  geom_errorbarh(aes(xmin=lower, xmax=upper, colour = network), height = 0.4, alpha = 0.5, show.legend = F)+
+  geom_point(aes(y = clustering, x = actual))+
+  facet_wrap(~metric, scales = 'free_x', ncol = 3)+
+  scale_colour_manual(values=cbPalette, name = 'Observed network\nvalue')+
   theme_dark()+
   theme(panel.border = element_blank(), panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))+
+  theme(legend.position="bottom")+
   labs(y = 'Clustering %', x = 'Metric value')
 metrics_facet
 
-pdf('plots/randomized_ranges/all_facet.pdf', width = 14)
+pdf('plots/randomized_ranges/all_facet.pdf', width = 12)
 metrics_facet
 dev.off()
 
