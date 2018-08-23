9bff60b038997f5c8331b20108624257e49596ad
hemprichbennett <hemprich.bennett@gmail.com>
2018-08-23 15:17:32 +0100

    Made initiation scripts

---
+++ scripts/r/in_netreducing_JUST_MODULARITY.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#$ -cwd     # Use current working directory
+#$ -V     # Verbose
+#$ -j y     # Maximum output, inc errors
+#$ -r y     # Condense error files
+#$ -pe smp 1     # Request CPU cores
+#$ -l h_rt=240:0:0     # Request runtime (up to 240 hours)
+#$ -l h_vmem=20G     # Request RAM per core
+#$ -m bea     # Status emails
+#$ -t 14-14
+
+
+module load R/3.4.3
+
+
+
+Rscript /data/scratch/btw863/bat-diet/scripts/r/netreducing.R ${SGE_TASK_ID}
