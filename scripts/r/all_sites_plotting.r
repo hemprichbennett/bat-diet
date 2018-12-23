77c4e88bfd685c8417e5b3e1514a043028f51a20
hemprichbennett <hemprich.bennett@gmail.com>
2018-12-23 16:51:55 +0000

    Gave nodes full species-names, made plots prettier

--- scripts/r/all_sites_plotting.r
+++ scripts/r/all_sites_plotting.r
@@ -1,14 +1,32 @@
 
-if(interactive()==TRUE){
-  library('here')
-}else{
-  library(here, lib.loc = '/data/home/btw863/r_packages/')
-}
+library(here)
+library(magrittr)  
 setwd(here())
 getwd()
 source('scripts/r/r_network_gen.r')
 
 nets <- r_network_gen(collapse_species = T, filter_species = T, lulu = T)
+
+names(nets) %<>%
+  gsub('DANUM', 'Danum', .)%<>%
+  gsub('MALIAU', 'Maliau', .)
+  
+
+for(i in 1:length(nets)){
+  colnames(nets[[i]]) %<>%
+    gsub('Hice', 'Hipposideros cervinus', .)%<>%
+    gsub('Hidi', 'Hipposideros diadema', .)%<>%
+    gsub('Hidy', 'Hipposideros dyacorum', .)%<>%
+    gsub('Hiri', 'Hipposideros ridleyi', .)%<>%
+    gsub('Keha', 'Kerivoula hardwickii', .)%<>%
+    gsub('Kein', 'Kerivoula intermedia', .)%<>%
+    gsub('Kepa', 'Kerivoula papillosa', .)%<>%
+    gsub('Kepe', 'Kerivoula pellucida', .)%<>%
+    gsub('Rhbo', 'Rhinolophus borneensis', .)%<>%
+    gsub('Rhse', 'Rhinolophus sedulus', .)%<>%
+    gsub('Rhtr', 'Rhinolophus trifoliatus', .)
+  
+}
 
 
 
@@ -29,6 +47,7 @@ for(n in 1:length(nets)){
   molten_net <- melt(net)
   molten_net_2 <- molten_net[(which(molten_net[,3] != 0)),]
   bat_igraph <- graph.data.frame(molten_net_2, directed = F)
+  #bat_igraph$layout <- layout_in_circle
   V(bat_igraph)$type <- V(bat_igraph)$name %in% molten_net_2[,1]
   igraph_list[[n]] <- bat_igraph
   
@@ -43,7 +62,7 @@ for(n in 1:length(nets)){
   
   V(bat_igraph)$name <- gsub('denovo.+', '', V(bat_igraph)$name)
   pdf(paste('plots/Site comparisons/', names(nets)[n], '.pdf', sep =''))
-  plot(bat_igraph, vertex.size = 10, vertex.color=node_cul, main = names(nets)[n])
+  plot(bat_igraph, vertex.size = 10, vertex.color=node_cul, main = names(nets)[n], width = 0.01, label.dist = 1)
   dev.off()
 }
 
